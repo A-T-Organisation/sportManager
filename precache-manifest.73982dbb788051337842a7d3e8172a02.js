9832a9883b196bfa7b09986d7a6aebef88b533ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-22 12:46:33 +0000

    Deploy A-T-Organisation/sportManager to github.com/A-T-Organisation/sportManager.git:gh-pages

---
+++ precache-manifest.73982dbb788051337842a7d3e8172a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fb02a23feae629e26e410ddf9a22a3",
+    "url": "/sportManager/index.html"
+  },
+  {
+    "revision": "a41e7daa0938421c8487",
+    "url": "/sportManager/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5040acc5986310e03230",
+    "url": "/sportManager/static/js/2.9dfdd2af.chunk.js"
+  },
+  {
+    "revision": "a41e7daa0938421c8487",
+    "url": "/sportManager/static/js/main.1489be8a.chunk.js"
+  },
+  {
+    "revision": "623c4f7668be5424e47c",
+    "url": "/sportManager/static/js/runtime~main.8bd108bb.js"
+  }
+]);
